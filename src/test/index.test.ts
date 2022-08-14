52d14417f19c5b12647b0e4bd097e27bcb7b39c9
orangegrove_1955 <orangegrove_1955@hotmail.com>
2022-08-14 11:28:24 +1000

    Added initial expectations from requirements doc

---
+++ src/test/index.test.ts
@@ -0,0 +1,81 @@
+describe('Download file', () => {
+  it('should successfully download file from URL', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('Process records', () => {
+  it('should exclude 0 total order value records from summary', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process order_id', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process order_datetime', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process total_order_value', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process average_unit_price', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process distinct_unit_count', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process total_units_count', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should process customer_state', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('Create output file', () => {
+  it('should produce an output file', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should be in CSV format', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should be well formed CSV', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include order_id', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include order_datetime', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include total_order_value', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include average_unit_price', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include distinct_unit_count', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include total_units_count', () => {
+    expect(true).toBe(false);
+  });
+
+  it('should include customer_state', () => {
+    expect(true).toBe(false);
+  });
+});
